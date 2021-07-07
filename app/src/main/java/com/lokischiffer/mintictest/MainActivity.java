0ef8d13ab53566407c18ea591127fbda92b4ff58
LokiSchiffer <norbertomosquera@usantotomas.edu.co>
2021-07-07 07:46:52 -0500

    Initialize repository

---
+++ app/src/main/java/com/lokischiffer/mintictest/MainActivity.java
@@ -0,0 +1,14 @@
+package com.lokischiffer.mintictest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_example);
+    }
+}
