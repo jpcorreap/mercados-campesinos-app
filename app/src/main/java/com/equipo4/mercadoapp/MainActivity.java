36d15c4859dffa5560d99207c3380567d40eb884
Viviana Chacon <apple@MacBook-Pro-de-Apple.local>
2021-07-17 20:25:46 -0500

    rollback changes

--- app/src/main/java/com/equipo4/mercadoapp/MainActivity.java
+++ app/src/main/java/com/equipo4/mercadoapp/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.register_main);
+        setContentView(R.layout.user_info);
     }
 }
