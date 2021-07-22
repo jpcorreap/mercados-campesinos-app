7273f37ee2f321846e6768a2a7b4f0a38c374e00
LokiSchiffer <norbertomosquera@usantotomas.edu.co>
2021-07-21 21:30:16 -0500

    Change main display to farmer register

--- app/src/main/java/com/equipo4/mercadoapp/MainActivity.java
+++ app/src/main/java/com/equipo4/mercadoapp/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.user_info);
+        setContentView(R.layout.farmer_register);
     }
 }
