c844f91811130bb94bdcd6d5c6ababb99006c955
LokiSchiffer <norbertomosquera@usantotomas.edu.co>
2021-07-30 14:57:39 -0500

    Implement interface objects for interaction

--- app/src/main/java/com/equipo4/mercadoapp/RegisterActivity.java
+++ app/src/main/java/com/equipo4/mercadoapp/RegisterActivity.java
@@ -3,7 +3,9 @@ package com.equipo4.mercadoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
 
@@ -17,5 +19,31 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.farmer_register);
+        
+        init();
+        events();
     }
+
+    private void events() {
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(password == passwordRepeat)
+                    Toast.makeText(RegisterActivity.this, "Listo para registrar", Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(RegisterActivity.this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void init(){
+        name = findViewById(R.id.name);
+        username = findViewById(R.id.username);
+        mail = findViewById(R.id.mail);
+        phone = findViewById(R.id.phone);
+        password = findViewById(R.id.password);
+        passwordRepeat = findViewById(R.id.passwordRepeat);
+        register = findViewById(R.id.registerFarmer);
+    }
+
 }
