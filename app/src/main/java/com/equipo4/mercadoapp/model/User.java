f06d3d8696585473a41bdab0a628b6a1cb6f796d
LokiSchiffer <norbertomosquera@usantotomas.edu.co>
2021-07-30 15:03:46 -0500

    Create model for User

---
+++ app/src/main/java/com/equipo4/mercadoapp/model/User.java
@@ -0,0 +1,56 @@
+package com.equipo4.mercadoapp.model;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private String personName, username, mail, password;
+    private int phone;
+
+    public User(String personName, String username, String mail, String password, int phone) {
+        this.personName = personName;
+        this.username = username;
+        this.mail = mail;
+        this.password = password;
+        this.phone = phone;
+    }
+
+    public String getPersonName() {
+        return personName;
+    }
+
+    public void setPersonName(String personName) {
+        this.personName = personName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+}
