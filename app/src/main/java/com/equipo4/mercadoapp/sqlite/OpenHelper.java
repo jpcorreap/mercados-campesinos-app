d2c52c28da2e3fdc29f05f53c8ba6f2e2a3d5ece
LokiSchiffer <norbertomosquera@usantotomas.edu.co>
2021-08-03 11:54:33 -0500

    Create sql file for connection

---
+++ app/src/main/java/com/equipo4/mercadoapp/sqlite/OpenHelper.java
@@ -0,0 +1,26 @@
+package com.equipo4.mercadoapp.sqlite;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class OpenHelper extends SQLiteOpenHelper {
+    public static final String DB_NAME = "db_mercado.db";
+    public static final int DB_VERSION = 1;
+
+    public OpenHelper(@Nullable Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
