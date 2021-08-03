f7d2a765d9331deac0457d2c1052c73722f2ec31
LokiSchiffer <norbertomosquera@usantotomas.edu.co>
2021-08-03 14:57:45 -0500

    Implement register function to local db

--- app/src/main/java/com/equipo4/mercadoapp/RegisterActivity.java
+++ app/src/main/java/com/equipo4/mercadoapp/RegisterActivity.java
@@ -3,17 +3,17 @@ package com.equipo4.mercadoapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.equipo4.mercadoapp.model.User;
 import com.equipo4.mercadoapp.sqlite.OpenHelper;
 import com.google.android.material.textfield.TextInputEditText;
 
 public class RegisterActivity extends AppCompatActivity {
 
     //UI
-    TextInputEditText name, username, mail, phone, password, passwordRepeat;
+    TextInputEditText name, username, email, phone, password, passwordRepeat;
     Button register;
 
     //SQLite
@@ -31,16 +31,28 @@ public class RegisterActivity extends AppCompatActivity {
     private void events() {
         register.setOnClickListener(v -> {
             if(password.getText().toString().equals(passwordRepeat.getText().toString()))
-                Toast.makeText(RegisterActivity.this, "Listo para registrar", Toast.LENGTH_SHORT).show();
+                //Toast.makeText(RegisterActivity.this, "Listo para registrar", Toast.LENGTH_SHORT).show();
+                registrarUser();
             else
                 Toast.makeText(RegisterActivity.this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show();
         });
     }
 
+    private void registrarUser() {
+        User user = new User(name.getText().toString(), username.getText().toString(),
+                email.getText().toString(), password.getText().toString(),
+                Integer.parseInt(phone.getText().toString()));
+        long rows = helper.insert(User.TABLE, user.getContentValues());
+        if(rows > 0)
+            Toast.makeText(this, "Se registro el usuario correctamente", Toast.LENGTH_SHORT).show();
+        else
+            Toast.makeText(this, "No se registro el usuario :C", Toast.LENGTH_SHORT).show();
+    }
+
     private void init(){
         name = findViewById(R.id.name);
         username = findViewById(R.id.username);
-        mail = findViewById(R.id.mail);
+        email = findViewById(R.id.mail);
         phone = findViewById(R.id.phone);
         password = findViewById(R.id.password);
         passwordRepeat = findViewById(R.id.passwordRepeat);
