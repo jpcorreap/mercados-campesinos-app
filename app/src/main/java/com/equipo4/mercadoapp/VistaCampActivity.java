1866102e00d62391d6b282be8075783f96c2b92b
aapocode <alonso.paezo@gmail.com>
2021-08-03 00:39:40 -0500

    Implements SearchView, RecyclerView methods, toast for the menu items

--- app/src/main/java/com/equipo4/mercadoapp/VistaCampActivity.java
+++ app/src/main/java/com/equipo4/mercadoapp/VistaCampActivity.java
@@ -1,20 +1,70 @@
 package com.equipo4.mercadoapp;
 //create el 26/07/2021 para probes
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.SearchView;
 import android.widget.Toast;
 
-public class VistaCampActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VistaCampActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
+    //*create el 02/08/2021 para probes
+    private List<ItemList> items;
+    private RecyclerView rvLista;
+    private RecyclerAdapter adapter;
+    private SearchView svSearch;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vistacamp);
+        //*create el 02/08/2021 para probes
+        initviews();
+        initValues();
+        initListener();
     }
+    //*create el 02/08/2021 para probes
+    private void initviews(){
+        rvLista = findViewById(R.id.recyclerView);
+        svSearch = findViewById(R.id.searchView);
+    }
+    //*create el 02/08/2021 para probes
+    private void initValues(){
+        LinearLayoutManager manager = new LinearLayoutManager(this);
+        rvLista.setLayoutManager(manager);
+
+        items = getItems();
+        adapter = new RecyclerAdapter(items);
+        rvLista.setAdapter(adapter);
+    }
+    //*create el 02/08/2021 para probes
+    private void initListener(){
+        svSearch.setOnQueryTextListener(this);
+    }
+
+    //*create el 02/08/2021 para probes
+    private List<ItemList> getItems() {
+        List<ItemList> itemLists = new ArrayList<>();
+        itemLists.add(new ItemList(R.drawable.logo_mercado_campesino,"1","Varios agricultores de la region invitados con muchos productos frescos","Parque El Divino Niño","Cra 32 #63a-15","agricultor1@agricultor.com","3001234567"));
+        itemLists.add(new ItemList(R.drawable.logo_mercado_campesino,"2","Varios agricultores de la region invitados con muchos productos frescos","Parque El Dorado","Cra 40 #20-135","agricultor2@agricultor.com","3011234567"));
+        itemLists.add(new ItemList(R.drawable.logo_mercado_campesino,"3","Varios agricultores de la region invitados con muchos productos frescos","Parque La Gloria","Cra 99 #80-75","agricultor3@agricultor.com","3021234567"));
+        itemLists.add(new ItemList(R.drawable.logo_mercado_campesino,"4","Varios agricultores de la region invitados con muchos productos frescos","Parque Libertadores","Calle 32 #45a-66","agricultor4@agricultor.com","3031234567"));
+        itemLists.add(new ItemList(R.drawable.logo_mercado_campesino,"5","Varios agricultores de la region invitados con muchos productos frescos","Parque La Montaña","Calle 60a #70-55","agricultor5@agricultor.com","3041234567"));
+
+        return itemLists;
+
+    }
+
 
     //*create el 25/07/2021 para probes
     @Override
@@ -34,12 +84,23 @@ public class VistaCampActivity extends AppCompatActivity {
                 Toast.makeText(this, "Clic desde la opcion SMS", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.navigation_news:
-                //Snackbar.make(this,item,"Clic desde la opcion news", BaseTransientBottomBar.LENGTH_SHORT).show();
+                Toast.makeText(this, "Clic desde la opcion Noticias", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.navigation_login:
-                //Snackbar.make(this,item,"Clic desde la opcion login", BaseTransientBottomBar.LENGTH_SHORT).show();
+                Toast.makeText(this, "Clic desde la opcion Login", Toast.LENGTH_SHORT).show();
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+    //*create el 02/08/2021 para probes
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        return false;
+    }
+    //*create el 02/08/2021 para probes
+    @Override
+    public boolean onQueryTextChange(String newText) {
+        adapter.filter(newText);
+        return false;
     }
 }
