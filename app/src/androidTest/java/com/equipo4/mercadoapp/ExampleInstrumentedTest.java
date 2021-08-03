c0896aca0b4cb869a69cb24f07ef73b3f887cc66
LokiSchiffer <norbertomosquera@usantotomas.edu.co>
2021-08-03 17:19:22 -0500

    Implement unitary tests

--- app/src/androidTest/java/com/equipo4/mercadoapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/equipo4/mercadoapp/ExampleInstrumentedTest.java
@@ -2,9 +2,15 @@ package com.equipo4.mercadoapp;
 
 import android.content.Context;
 
+import androidx.test.filters.LargeTest;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import com.equipo4.mercadoapp.model.User;
+import com.equipo4.mercadoapp.sqlite.OpenHelper;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -16,11 +22,37 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
+@LargeTest
 public class ExampleInstrumentedTest {
+//    @Test
+//    public void useAppContext() {
+//        // Context of the app under test.
+//        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+//        assertEquals("com.equipo4.mercadoapp", appContext.getPackageName());
+//    }
+    private OpenHelper helper;
+
+    @Before
+    public void setup(){
+        helper = new OpenHelper(InstrumentationRegistry.getInstrumentation().getTargetContext());
+        helper.openDB();
+    }
+    @After
+    public void finish(){
+        helper.closeDB();
+    }
+
     @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.equipo4.mercadoapp", appContext.getPackageName());
+    public void validateNull(){
+        assertNotNull(helper);
+    }
+
+    @Test
+    public void insertUser(){
+        User user = new User("Pepito Perez", "Pepito35", "pepito35@hotmail.com",
+                "prueba51", Long.parseLong("3101234567"));
+        long rows = helper.insert(User.TABLE, user.getContentValues());
+        //assertNotEquals(-1, rows);
+        assertEquals(rows, -1);
     }
 }
